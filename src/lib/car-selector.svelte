d399475985563d1dce399c92b9287c1b773554c0
Chanakan Mungtin <chanakan5591@chanakancloud.net>
2023-09-28 19:50:24 +0700

    added color selection

---
+++ src/lib/car-selector.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	export let selectedColor: 'grey' | 'red' | 'blue' | 'black' = 'blue';
+	import BlueCar from '$lib/amg-blue.png';
+	import GreyCar from '$lib/amg-grey.png';
+	import BlackCar from '$lib/amg-black.png';
+
+	const cars = {
+		blue: BlueCar,
+		grey: GreyCar,
+		black: BlackCar
+	};
+</script>
+
+<div class="w-[70%]">
+	<img src={cars[selectedColor]} alt={`Car with ${selectedColor} color`} />
+</div>
--- src/routes/cars/+page.svelte
+++ src/routes/cars/+page.svelte
@@ -1,49 +1,70 @@
 <script lang="ts">
-    import MenuIconButton from '$lib/components/menuiconbutton.svelte'
-    import Button from '$lib/components/button.svelte'
-    import { Info } from 'lucide-svelte'
-    import CarImg from '$lib/amg-blue.png'
+	import MenuIconButton from '$lib/components/menuiconbutton.svelte';
+	import Button from '$lib/components/button.svelte';
+	import { Info } from 'lucide-svelte';
+	import CarSelector from '$lib/car-selector.svelte';
+
+	let color: string = 'blue';
+
+	const setColor = (setcolor) => {
+		color = setcolor;
+		console.log(color);
+	};
 </script>
 
 <div class="pt-8 h-full w-full flex flex-col">
-    <div class="flex gap-x-4 items-center">
-        <span class="text-4xl font-bold">Mercedes-Benz</span>
-        <MenuIconButton><Info /></MenuIconButton>
-    </div>
-    <span class="text-xl text-gray-500">AMG C63 Coupe - 2023</span>
-    <div class="mx-auto mt-4 flex items-center justify-center h-[70%] w-[70%]">
-        <div class="flex flex-col gap-y-4">
-            <Button>Performance</Button>
-            <Button active={true}>Design</Button>
-            <Button>Safety</Button>
-            <Button>Luxury</Button>
-            <Button>Multimedia</Button>
-        </div>
-        <img src={CarImg} />'
-    </div>
-    <div class="flex mt-[-4rem] justify-between">
-        <div class='flex items-center gap-4'>
-            <span class="text-3xl ml-16 font-bold">Featured Highlights</span>
-            <MenuIconButton><Info /></MenuIconButton>
-        </div>
-        <div class="flex items-center justify-center">
-            <span class="text-3xl">Colors</span>
-            <div class="flex ml-4 gap-x-1 items-center justify-center">
-                <div class="rounded-full bg-gray-400 h-6 w-6">
-
-                </div>
-                <div class="rounded-full bg-red-700 w-6 h-6">
-                </div>
-                <div class="rounded-full w-9 h-9 flex border-gray-500 border-solid border-2 items-center justify-center">
-                    <div class="rounded-full bg-blue-600 w-6 h-6">
-
-                    </div>
-                </div>
-                <div class="rounded-full bg-black h-6 w-6">
-
-                </div>
-            </div>
-        </div>
-    </div>
-    <span class="ml-16 text-2xl">Coming Soon!</span>
+	<div class="flex gap-x-4 items-center">
+		<span class="text-4xl font-bold">Mercedes-Benz</span>
+		<MenuIconButton><Info /></MenuIconButton>
+	</div>
+	<span class="text-xl text-gray-500">AMG C63 Coupe - 2023</span>
+	<div class="mx-auto mt-4 flex items-center justify-between h-[70%] w-[90%]">
+		<div class="flex flex-col gap-y-4">
+			<Button>Performance</Button>
+			<Button active={true}>Design</Button>
+			<Button>Safety</Button>
+			<Button>Luxury</Button>
+			<Button>Multimedia</Button>
+		</div>
+		<CarSelector selectedColor={color} />
+	</div>
+	<div class="flex mt-[-4rem] justify-between">
+		<div class="flex items-center gap-4">
+			<span class="text-3xl ml-16 font-bold">Featured Highlights</span>
+			<MenuIconButton><Info /></MenuIconButton>
+		</div>
+		<div class="flex items-center justify-center">
+			<span class="text-3xl">Colors</span>
+			<div class="flex ml-4 items-center justify-center">
+				<div
+					class:invisible={color != 'grey'}
+					class="rounded-full w-9 h-9 flex border-gray-500 border-solid border-2 items-center justify-center"
+				>
+					<div
+						on:click={() => setColor('grey')}
+						class="visible cursor-pointer rounded-full bg-gray-400 h-6 w-6"
+					/>
+				</div>
+				<div
+					class:invisible={color != 'blue'}
+					class="rounded-full w-9 h-9 flex border-gray-500 border-solid border-2 items-center justify-center"
+				>
+					<div
+						on:click={() => setColor('blue')}
+						class="visible cursor-pointer rounded-full bg-blue-600 w-6 h-6"
+					/>
+				</div>
+				<div
+					class:invisible={color != 'black'}
+					class="rounded-full w-9 h-9 flex border-gray-500 border-solid border-2 items-center justify-center"
+				>
+					<div
+						on:click={() => setColor('black')}
+						class="visible cursor-pointer rounded-full bg-black h-6 w-6"
+					/>
+				</div>
+			</div>
+		</div>
+	</div>
+	<span class="ml-16 text-2xl">Coming Soon!</span>
 </div>
