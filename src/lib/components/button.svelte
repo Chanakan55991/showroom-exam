af038e09d39d1a4102f64e4fa0be05138eed6b2b
Chanakan Mungtin <chanakan5591@chanakancloud.net>
2023-09-28 20:35:08 +0700

    added cool stuff

--- src/lib/components/button.svelte
+++ src/lib/components/button.svelte
@@ -1,7 +1,15 @@
 <script lang="ts">
-        export let active = false
+	import toast from 'svelte-french-toast';
+
+	export let active = false;
 </script>
 
-<span class:bg-black={active} class:text-white={active} class="cursor-pointer border-gray-500 border-solid border-2 rounded-full w-36 px-5 bg-[#f2e4d0] text-center py-3">
-        <slot />
+<span
+	on:click={() => toast.error('Coming Soon!')}
+	class:bg-black={active}
+	class:text-white={active}
+	class="cursor-pointer border-gray-500 border-solid border-2 rounded-full w-36 px-5 bg-[#f2e4d0] text-center py-3"
+>
+	<slot />
 </span>
+
