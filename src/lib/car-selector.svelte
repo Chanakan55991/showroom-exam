0ebcb158d9c2162dbae3ecc6cf8528c5addb087a
Chanakan Mungtin <chanakan5591@chanakancloud.net>
2023-09-28 20:16:07 +0700

    upgraded stuff

--- src/lib/car-selector.svelte
+++ src/lib/car-selector.svelte
@@ -1,16 +1,18 @@
 <script lang="ts">
-	export let selectedColor: 'grey' | 'red' | 'blue' | 'black' = 'blue';
+	export let selectedColor: 'grey' | 'blue' | 'black' | 'white' = 'blue';
 	import BlueCar from '$lib/amg-blue.png';
 	import GreyCar from '$lib/amg-grey.png';
 	import BlackCar from '$lib/amg-black.png';
+	import WhiteCar from '$lib/amg-white.png';
 
 	const cars = {
 		blue: BlueCar,
 		grey: GreyCar,
-		black: BlackCar
+		black: BlackCar,
+		white: WhiteCar
 	};
 </script>
 
-<div class="w-[70%]">
+<div class="w-[90%]">
 	<img src={cars[selectedColor]} alt={`Car with ${selectedColor} color`} />
 </div>
--- src/routes/cars/+page.svelte
+++ src/routes/cars/+page.svelte
@@ -1,21 +1,21 @@
 <script lang="ts">
 	import MenuIconButton from '$lib/components/menuiconbutton.svelte';
 	import Button from '$lib/components/button.svelte';
-	import { Info } from 'lucide-svelte';
+	import { Info, Award } from 'lucide-svelte';
 	import CarSelector from '$lib/car-selector.svelte';
 
-	let color: string = 'blue';
-
+	let color: 'grey' | 'blue' | 'black' | 'white' = 'blue';
 	const setColor = (setcolor) => {
 		color = setcolor;
-		console.log(color);
 	};
 </script>
 
 <div class="pt-8 h-full w-full flex flex-col">
 	<div class="flex gap-x-4 items-center">
 		<span class="text-4xl font-bold">Mercedes-Benz</span>
-		<MenuIconButton><Info /></MenuIconButton>
+		<a href="https://www.mbusa.com/en/vehicles/model/c-class/coupe/c63cs#key-features">
+			<MenuIconButton><Info /></MenuIconButton>
+		</a>
 	</div>
 	<span class="text-xl text-gray-500">AMG C63 Coupe - 2023</span>
 	<div class="mx-auto mt-4 flex items-center justify-between h-[70%] w-[90%]">
@@ -26,13 +26,36 @@
 			<Button>Luxury</Button>
 			<Button>Multimedia</Button>
 		</div>
-		<CarSelector selectedColor={color} />
+
+		<div class="flex justify-center">
+			<CarSelector selectedColor={color} />
+		</div>
+
+		<div class="absolute flex-col items-center flex w-[20rem] mt-[-16rem] right-8">
+			<div
+				class="bg-black text-white w-full flex items-center justify-center py-6 rounded-xl mb-[-1rem]"
+			>
+				<div class="rounded-full bg-gray-800 p-3 mr-2">
+					<Award size={24} />
+				</div>
+				<span class="text-xl">Unsurpassed Power</span>
+			</div>
+			<div
+				class="p-4 rounded-xl border-black border-2 border-solid bg-white bg-opacity-50 backdrop-blur-md self-start"
+			>
+				<span>
+					A handcrafted 503-hp biturbo V8 unleashes a 516-lb-ft torrent of torque. Aggressive style
+					envelops advanced technologies.
+				</span>
+			</div>
+		</div>
 	</div>
 	<div class="flex mt-[-4rem] justify-between">
 		<div class="flex items-center gap-4">
 			<span class="text-3xl ml-16 font-bold">Featured Highlights</span>
 			<MenuIconButton><Info /></MenuIconButton>
 		</div>
+
 		<div class="flex items-center justify-center">
 			<span class="text-3xl">Colors</span>
 			<div class="flex ml-4 items-center justify-center">
@@ -61,6 +84,15 @@
 					<div
 						on:click={() => setColor('black')}
 						class="visible cursor-pointer rounded-full bg-black h-6 w-6"
+					/>
+				</div>
+				<div
+					class:invisible={color != 'white'}
+					class="rounded-full w-9 h-9 flex border-gray-500 border-solid border-2 items-center justify-center"
+				>
+					<div
+						on:click={() => setColor('white')}
+						class="visible cursor-pointer rounded-full bg-white h-6 w-6"
 					/>
 				</div>
 			</div>
