ef1f849d45d91f98cd12ede4c58da9c2a4e7073c
Chanakan Mungtin <chanakan5591@chanakancloud.net>
2023-09-29 05:19:30 +0700

    login

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,14 @@
 <script lang="ts">
-    import Navbar from '$lib/components/navbar.svelte'
-    import '../style.pcss'
+	import Navbar from '$lib/components/navbar.svelte';
+	import '../style.pcss';
+	import { Toaster } from 'svelte-french-toast';
 </script>
 
 <main class="w-[96%] mx-auto flex h-screen">
-    <Navbar />
-    <div class="pt-24 h-full flex w-screen">
-        <slot />
-    </div>
+	<Toaster />
+	<Navbar />
+	<div class="pt-24 h-full flex w-screen">
+		<slot />
+	</div>
 </main>
+
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,9 +1,27 @@
-<div class="flex flex-col h-full w-full items-center justify-center">
-    <span class="text-4xl mb-4">Login</span>
-    <label for='username'>Username</label>
-    <input type="text" name="username">
-    <label for="password">Password</label>
-    <input type="password">
+<script lang="ts">
+	import toast from 'svelte-french-toast';
+</script>
 
-    <button type="submit" class="mt-4 bg-amber-300">Button</button>
+<div class="flex h-full w-full items-center justify-center">
+	<div class="w-64 flex flex-col">
+		<span class="text-4xl mb-4">Login</span>
+		<label for="username">Username</label>
+		<input
+			type="text"
+			class="p-2 border-black border-2 border-solid rounded-full"
+			name="username"
+		/>
+		<label for="password" class="mt-2">Password</label>
+		<input
+			type="password"
+			class="p-2 border-black border-2 border-solid rounded-full"
+			name="password"
+		/>
+
+		<button
+			on:click={() => toast.error('Coming Soon!')}
+			type="submit"
+			class="mt-4 border-black border-2 border-solid p-2 px-4 w-full rounded-full">Login</button
+		>
+	</div>
 </div>
